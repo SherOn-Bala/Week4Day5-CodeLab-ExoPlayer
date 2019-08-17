ec729ce860608f611c9d2b597131fd8e758d1936
SherOn-Bala <jude.balasingam@mobileappscompany.us>
2019-08-17 19:16:57 -0400

    finished codelab

---
+++ README.md
@@ -0,0 +1,10 @@
+## ExoPlayer ##
+## Screenshots ##
+<p float="left">
+  <img src="/screenshots/1.png" width="200" />
+  <img src="/screenshots/2.png" width="200" />
+  <img src="/screenshots/3.png" width="200" />
+</p>
+
+## Code Instructions ##
+### [ExoPlayer CodeLab](https://codelabs.developers.google.com/codelabs/exoplayer-intro/#0) ###
---
+++ demos/cast/buildout/generated/source/buildConfig/androidTest/debug/com/google/android/exoplayer2/castdemo/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.google.android.exoplayer2.castdemo.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.google.android.exoplayer2.castdemo.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 2010004;
+  public static final String VERSION_NAME = "2.10.4";
+}
